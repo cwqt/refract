c0ffc6bca17b0a385af3ce7ed1be9b3cb6a8a40a
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2022-04-30 19:52:40 +0200

    feat: split align method into alignKv and alignFields

--- src/codegen/align.ts
+++ src/codegen/align.ts
@@ -1,6 +1,7 @@
-// Aligns contents of a model/generator to that of the longest column
-export const align = (value: string, delimiter: string = ' '): string => {
+// Align contents of a datasource/generator to that of the longest column
+export const alignKv = (value: string): string => {
   const lines = value.split('\n');
+  const delimiter = '=';
 
   const max =
     Math.max(...lines.map(l => l.trim().split(delimiter).shift().length)) + 1;
@@ -9,6 +10,30 @@ export const align = (value: string, delimiter: string = ' '): string => {
     .map(line => line.split(delimiter))
     .map(([head, ...rest]) =>
       [head.padEnd(max), delimiter, ...rest.map(v => v.trim())].join(' '),
+    )
+    .join('\n');
+};
+
+// Align contents of a model to that of the longest column
+export const alignFields = (value: string): string => {
+  const lines = value.split('\n');
+
+  let maximumColumnName = 0,
+    MaximumColumnType = 0;
+  for (const line of lines) {
+    const [columnName, columnType] = line.split(' ');
+    maximumColumnName = Math.max(maximumColumnName, columnName.length);
+    MaximumColumnType = Math.max(MaximumColumnType, (columnType ?? '').length);
+  }
+
+  return lines
+    .map(line => line.split(' '))
+    .map(([columnName, columnType, ...rest]) =>
+      [
+        columnName.padEnd(maximumColumnName + 1),
+        columnType ? columnType.padEnd(MaximumColumnType + 1) : '',
+        ...rest.map(v => v.trim()),
+      ].join(' '),
     )
     .join('\n');
 };
