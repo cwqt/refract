afa7ebfe6740c82ce9424bf777573a2c9fb9365c
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2022-06-04 17:06:54 +0200

    feat: add support to CockroachDB native modifiers

---
+++ src/public/db/cockroach.ts
@@ -0,0 +1,29 @@
+import { db } from './utils';
+
+// https://www.prisma.io/docs/concepts/database-connectors/postgresql
+const _ = db('cockroachdb');
+
+// prettier-ignore
+export default {
+  Int8:                                          _.BigInt('Int8'),
+  Bool:                                          _.Boolean('Bool'),
+  TimeStamp: (precision?: number) =>             _.DateTime('Timestamp', precision),
+  Timestamptz: (precision?: number) =>           _.DateTime('Timestamptz', precision),
+  Time: (precision?: number) =>                  _.DateTime('Time', precision),
+  Timetz: (precision?: number) =>                _.DateTime('Timetz', precision),
+  Decimal: (precision: number, scale: number) => _.Decimal('Decimal', [precision, scale]),
+  Float4:                                        _.Float('Float4'),
+  Float8:                                        _.Float('Float8'),
+  Int2:                                          _.Int('Int2'),
+  Int4:                                          _.Int('Int4'),
+  Char: (length?: number) =>                     _.String('Char', length),
+  CatalogSingleChar:                             _.String('CatalogSingleChar'),
+  String:                                        _.String('String'),
+  Date:                                          _.DateTime('Date'),
+  Inet:                                          _.String('Inet'),
+  Bit: (length?: number) =>                      _.String('Bit', length),
+  VarBit: (length?: number) =>                   _.String('VarBit', length),
+  Oid:                                           _.Int('Oid'),
+  Uuid:                                          _.String('Uuid'),
+  JsonB:                                         _.Json('JsonB'),
+};
--- src/public/db/index.ts
+++ src/public/db/index.ts
@@ -1,3 +1,4 @@
 export { default as Mongo } from './mongo';
 export { default as MySql } from './mysql';
 export { default as Postgres } from './postgresql';
+export { default as Cockroach } from './cockroach';
